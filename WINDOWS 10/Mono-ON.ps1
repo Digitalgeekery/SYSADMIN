08045aaedbabfee3a4d174a3758c2a5f0b36e1c9
DIGITALGEEREKY <mdoherty51@gmail.com>
2024-09-11 10:32:55 +0100

    Create Mono-ON.ps1

---
+++ WINDOWS 10/Mono-ON.ps1
@@ -0,0 +1,22 @@
+<#
+.SYNOPSIS
+  Updates the audio settings and restarts the audio service.
+.DESCRIPTION
+  This script modifies a registry value related to audio accessibility, then restarts the Windows audio service to apply the change.
+.PARAMETER None
+.INPUTS
+  None
+.OUTPUTS
+  None
+.NOTES
+  Version:        1.0
+  Author:         DIGITALGEEKERY
+  Creation Date:  11/09/2024
+  Purpose/Change: INITIAL SCRIPT DEVELOPMENT
+  Requires:       Administrative privileges.
+
+.EXAMPLE
+  powershell -windowstyle hidden -command "Start-Process cmd -ArgumentList '/s,/c,REG ADD "HKCU\Software\Microsoft\Multimedia\Audio" /V AccessibilityMonoMixState /T REG_DWORD /D 1 /F & net stop "Audiosrv" & net start "Audiosrv"' -Verb runAs"
+#>
+
+powershell -windowstyle hidden -command "Start-Process cmd -ArgumentList '/s,/c,REG ADD \"HKCU\Software\Microsoft\Multimedia\Audio\" /V AccessibilityMonoMixState /T REG_DWORD /D 1 /F & net stop \"Audiosrv\" & net start \"Audiosrv\"' -Verb runAs"
